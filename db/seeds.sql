1d0e50e02e06c444496dc17e51376063b2ff916c
bvsswave <tannershahan@gmail.com>
2022-03-07 11:12:17 -0700

    Adding values to seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO department(department_name)
+VALUES("Photography"), ("Marketing"), ("Reception"), ("Engineering"), ("Customer Service");
+INSERT INTO role(title, salary, department_id)
+VALUES
