441cc0ec8a4954d2435f263fead42d614b0362f5
bvsswave <tannershahan@gmail.com>
2022-03-07 11:30:46 -0700

    Updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,23 +1,23 @@
-
--- department table
-
+DROP DATABASE IF EXISTS employees;
+CREATE DATABASE employees;
+USE employees;
+-- Department table
 CREATE TABLE department (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
     department_name VARCHAR(30) NOT NULL
 );
--- role table
-
+-- Role table
 CREATE TABLE role (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10, 2) NOT NULL,
     department_id INTEGER
 );
--- employee table
+-- Employee table
 CREATE TABLE employee(
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER,
     manager_id INTEGER
-)
+);
