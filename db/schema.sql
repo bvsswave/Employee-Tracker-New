6bf4742fb151d28a8115b5d07124625b772332b5
bvsswave <tannershahan@gmail.com>
2022-03-07 11:01:45 -0700

    Added logic that was suggested in module

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,23 @@
+
+-- department table
+
 CREATE TABLE department (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
     department_name VARCHAR(30) NOT NULL
 );
+-- role table
+
+CREATE TABLE role (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10, 2) NOT NULL,
+    department_id INTEGER
+);
+-- employee table
+CREATE TABLE employee(
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    manager_id INTEGER
+)
