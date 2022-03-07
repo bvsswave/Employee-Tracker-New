ea209aeb1345212a9aa91069cf9addeb71222473
bvsswave <tannershahan@gmail.com>
2022-03-07 11:45:42 -0700

    Updated validate.js and server.js

---
+++ javascript/validate.js
@@ -0,0 +1,19 @@
+const validator = require('validator');
+const validate = {
+  validateString(str) {
+    return str !== '' || 'ENTER VALID RESPONSE';
+  },
+
+  validateSalary(num) {
+    if (validator.isDecimal(num)) return true;
+    return 'ENTER VALID SALARY';
+  },
+
+  isSame(str1, str2) {
+    if (str1 === str2) return true;
+  }
+};
+
+
+
+module.exports = validate;
--- server.js
+++ server.js
@@ -1,1 +1,6 @@
+const connection = require('./config/connection');
 const figlet = require('figlet');
+const validate = require('./javascript/validate');
+const chalk = require('chalk');
+const inquirer = require('inquirer');
+const validate = require('./javascript/validate');
