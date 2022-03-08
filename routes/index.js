7c0fd0bf7d7173a2bea2efc96194ca82f6a40041
bvsswave <tannershahan@gmail.com>
2022-03-07 17:31:56 -0700

    Updating index.js

--- routes/index.js
+++ routes/index.js
@@ -16,22 +16,83 @@ async function startQuestions() {
             type: 'list',
             name: 'trackerAction',
             message: "Where to now?",
-            choices: ['View all departments', 'View all roles', 'View all employees', 'Add a role', 'Add an employee', 'Add a department', 'Update employee role', 'Quit']
+            choices: ['Display all departments', 'Display all roles', 'Display all employees', 'Add a role', 'Add an employee', 'Add a department', 'Update employee role', 'Quit']
         }
     ])
 
 
 
-    if (question.trackerAction === 'View all departments') {
+    if (question.trackerAction === 'Display all departments') {
         viewDepartments();
 
     }
-     if (question.trackerAction === "View all roles") {
+     if (question.trackerAction === "Display all roles") {
         viewRoles();
         
     }
 
-    if (question.trackerAction === "View all employees") {
+    if (question.trackerAction === "Display all employees") {
         viewEmployees();
        
     }
+
+    let newRole
+    if (question.trackerAction === "Add a role") {
+        newRole = await inquirer.prompt([
+            {
+                type: 'input',
+                name: 'newRoleTitle',
+                message: "Enter title of new role",
+                validate: newRoleTitle => {
+                    if (newRoleTitle) {
+                        return true;
+                    } else {
+                        console.log("Please enter title of new role")
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'newRoleSalary',
+                message: "How much is the salary for the new role?",
+                validate: newRoleSalary => {
+                    if (newRoleSalary) {
+                        return true;
+                    } else {
+                        console.log("Please enter a salary for the new role.")
+                    }
+                }
+            },
+            {
+                type: 'list',
+                name: 'departmentID',
+                message: "Which departmnet will the new role be in?",
+                choices: ['1', '2', '3', '4']
+            }
+
+        ])
+
+        // Pushing the array to addRole
+        if (newRole) {
+            roles.push(newRole)
+        }
+
+        addRole();
+        
+    }
+
+    if (question.trackerAction === "Add an employee") {
+        
+        newEmployee = await inquirer.prompt([
+            {
+                type: 'input',
+                name: 'newEmployeeFirstName',
+                message: "What is the first name of your new employee?",
+                validate: newEmployeeFirstName => {
+                    if (newEmployeeFirstName) {
+                        return true;
+                    } else {
+                        console.log("Please enter the first name of the employee.")
+                    }
+                }
+            },
